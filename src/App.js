38a305f34b184cb4bd22b42a732613372fae4ee2
Maria Loriana Porumb <e1429844@student.tuwien.ac.at>
2021-02-07 18:50:52 +0100

    added progress bar (no dynamic progress yet)

--- src/App.js
+++ src/App.js
@@ -2,65 +2,101 @@ import React, { Component } from 'react';
 import './App.css';
 import Tabset from './Tabset';
 
-import {parseDiff, Diff, Hunk, tokenize} from 'react-diff-view';
+import { parseDiff, Diff, Hunk, tokenize } from 'react-diff-view';
 import 'react-diff-view/style/index.css';
+
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import ProgressBar from "@ramonak/react-progress-bar";
 
 import axios from 'axios';
 
+//add pretty progress bar: https://reactjsexample.com/a-library-to-create-stunning-progress-bars-and-steps-in-react/
+//or this one with a label: https://www.npmjs.com/package/@ramonak/react-progress-bar
 
 class App extends Component {
-  state = {  
+  state = {
     next_sha: null,
+    message: null,
     files: []
   }
 
   componentDidMount() {
 
     axios.get('http://localhost:3030/commits')
-    .then(response => {
-      console.log("IN COMPONENT DID MOUNT")
-      console.log(parseDiff(response.data.files))
-      this.setState({
-        next_sha: response.data.next_sha,
-        files: parseDiff(response.data.files)
+      .then(response => {
+        console.log("IN COMPONENT DID MOUNT")
+        console.log(parseDiff(response.data.files))
+        this.setState({
+          next_sha: response.data.next_sha,
+          message: response.data.message,
+          files: parseDiff(response.data.files)
+        })
       })
-    })
-    .catch(error => {
-      console.log(error)
-    })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
   handleNext = () => {
     console.log("Call me! Call me any, any time")
     axios.get(`http://localhost:3030/commits/${this.state.next_sha}`)
-    .then(response => {
-      console.log(response)
-      this.setState({
-        next_sha: response.data.next_sha,
-        files: parseDiff(response.data.files)
+      .then(response => {
+        console.log(response)
+        this.setState({
+          next_sha: response.data.next_sha,
+          message: response.data.message,
+          files: parseDiff(response.data.files)
+        })
       })
-    })
-    .catch(error => {
-      console.log(error)
-    })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
-  
 
-  render() { 
-    return (  
-      <div className="App"> 
-        <button onClick={this.handleNext }>NEEEEXT</button>
+
+  render() {
+    return (
+      <div className="App">
+        <ProgressBar
+          completed={60}
+          bgcolor="#6a1b9a"
+          baseBgColor="#e0e0de"
+          margin="10px"
+        />
+        <Jumbotron>
+          <Container>
+            <Row>
+              <Col sm={9}><p>{this.state.message}</p></Col>
+              <Col sm={2}>
+                <ButtonGroup vertical>
+                  <Button onClick={this.handleNext} variant="primary">NEEEEXT</Button>
+                  <Button onClick={this.handleNext} variant="info" >THEORY</Button>
+                </ButtonGroup>
+              </Col>
+            </Row>
+
+          </Container>
+
+        </Jumbotron>
+
         {
-          this.state.files.length > 0?
-          <Tabset files={this.state.files}></Tabset>
-          :
-          <p>Loading ...</p>
+          this.state.files.length > 0 ?
+            <Tabset files={this.state.files}></Tabset>
+            :
+            <p>Loading ...</p>
         }
-    </div>
-    ); 
+      </div>
+    );
 
   }
 }
- 
+
 export default App;
--- src/Tabset.js
+++ src/Tabset.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { Tabs, Tab } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {parseDiff, Diff, Hunk, tokenize} from 'react-diff-view';
+import { parseDiff, Diff, Hunk, tokenize } from 'react-diff-view';
 import 'react-diff-view/style/index.css';
 import refractor from 'refractor';
 
@@ -13,48 +13,53 @@ class Tabset extends Component {
 
   renderToken = (token, defaultRender, i) => {
     switch (token.type) {
-        case 'space':
-            console.log(token);
-            return (
-                <span key={i} className="space">
-                    {token.children && token.children.map((token, i) => renderToken(token, defaultRender, i))}
-                </span>
-            );
-        default:
-            return defaultRender(token, i);
+      case 'space':
+        console.log(token);
+        return (
+          <span key={i} className="space">
+            {token.children && token.children.map((token, i) => renderToken(token, defaultRender, i))}
+          </span>
+        );
+      default:
+        return defaultRender(token, i);
     }
-};
+  };
 
-customize_options(file) {
-  let file_path = file.newPath.split('.')
-  let extension = file_path[file_path.length-1]
+  customize_options(file) {
+    let file_path = file.newPath.split('.')
+    let extension = file_path[file_path.length - 1]
 
-  let options = {
-    highlight: true,
-    refractor: refractor,
-    language: extension
-   }
-  return options
-}
+    let options = {
+      highlight: true,
+      refractor: refractor,
+      language: extension
+    }
+    return options
+  }
+
+  get_active_tab(files) {
+    console.log(`ACTIVE PATH: ${files[0].newPath}`)
+    return files[0].newPath
+  }
 
   render() {
     let { files } = this.props
 
     return (
 
-      <Tabs defaultActiveKey={ files[0].newPath }>
-      {
-        files.map(file => 
-        <Tab  key={file.newPath} eventKey={file.newPath} title={file.newPath}>
-          <div key={file.newPath} className="tab-item-wrapper">
-              <Diff 
-          key={file.oldRevision + '-' + file.newRevision} 
-          viewType="unified" diffType={file.type} 
-          hunks={file.hunks} 
-          tokens={tokenize(file.hunks, this.customize_options(file))}></Diff>
-          </div>
-        </Tab>)
-      }
+      <Tabs defaultActiveKey={this.get_active_tab(files)}>
+        {
+          files.map(file =>
+            <Tab key={file.newPath} eventKey={file.newPath} title={file.newPath}>
+              <div key={file.newPath} className="tab-item-wrapper">
+                <Diff
+                  key={file.oldRevision + '-' + file.newRevision}
+                  viewType="unified" diffType={file.type}
+                  hunks={file.hunks}
+                  tokens={tokenize(file.hunks, this.customize_options(file))}></Diff>
+              </div>
+            </Tab>)
+        }
       </Tabs>
     );
   }
